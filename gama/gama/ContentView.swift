5ba0cdf96d5297681f9a89130f574286d24f426c
fatamah <fatiabbasali2018@gmail.com>
2022-03-02 20:35:46 +0300

    cw 22

---
+++ gama/gama/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gama
+//
+//  Created by fati on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("hello my name is  fatamah")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gama/gama/gamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  gamaApp.swift
+//  gama
+//
+//  Created by fati on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct gamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
